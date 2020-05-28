6141df0b14ee4875160beb02397299bdc6a06cc4
ivandi1980 <ivandi.djoh@gmail.com>
2020-05-28 17:24:23 +0700

    added useForm

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
-import React, { Component } from "react";
-import useForm from "react-hook-form";
+import React from "react";
+import { useForm } from "react-hook-form";
 import "./App.css";
 
 const { app } = window.require("electron").remote;
 
-export default function App() {
+const App = () => {
+  const { register, handleSubmit, errors } = useForm();
+
+  const onSubmit = (data) => {
+    alert(data.url.repeat(30));
+  };
+
   return (
     <div className="container">
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <input
           type="text"
           className="form-control mr-sm-2"
           placeholder="paste you url"
           name="url"
+          ref={register({
+            require: "URL is Invalid",
+            minLength: { value: 8, message: "too short" },
+          })}
         />
-        <input type="submit" className="btn btn-primary my-2 my-sm-0" />
+        {errors.url && <p>{errors.url.message}</p>}
+        <br />
+        <input type="submit" className="btn btn-secondary my-2 my-sm-0" />
       </form>
     </div>
   );
-}
+};
+
+export default App;
