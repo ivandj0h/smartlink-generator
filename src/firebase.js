b7331e50e270415f62af80208dbff553f9407f54
ivandi1980 <ivandi.djoh@gmail.com>
2020-05-30 01:15:52 +0700

    updating firebase enviroments

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,18 @@
-// import firebase from "firebase/app";
 import * as firebase from "firebase/app";
 import "firebase/analytics";
 import "firebase/firestore";
 
-// Your web app's Firebase configuration
 var firebaseConfig = {
-  apiKey: "AIzaSyDHyOWtQEl7kNAP8sD5F67F_Ih0B2yrw24",
-  authDomain: "electron-react.firebaseapp.com",
-  databaseURL: "https://electron-react.firebaseio.com",
-  projectId: "electron-react",
-  storageBucket: "electron-react.appspot.com",
-  messagingSenderId: "430325178667",
-  appId: "1:430325178667:web:a4d8c8d9fda45462f8e56e",
-  measurementId: "G-406R853S6D",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
-// Initialize Firebase
+
 firebase.initializeApp(firebaseConfig);
 firebase.analytics();
 
